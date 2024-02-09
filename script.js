a0e1621eccac66eda5cdc49d39cb535f840fabc8
loshpek228 <sasatagaev9@gmail.com>
2024-02-09 16:24:46 +0600

    initial

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let input = prompt("Введите значение");
+
+
+if (!isNaN(input )) {
+    console.log("Вы ввели число" + input);
+    alert = +prompt("Вы ввели число")
+}{
+    console.log("Введено некорректное значение");
+    alert = +prompt("Введено некорректное значение");
+}
